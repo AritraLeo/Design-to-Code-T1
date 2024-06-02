7e885e8f20e8b29868b9f0797e9dc5efe2c2278b
AritraLeo <aritrabinance056@gmail.com>
2024-06-02 11:58:51 +0530

    Check box and modal done

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 document.getElementById('contact-button').addEventListener('click', function () {
     console.log('Button clicked'); // Debugging
-    document.getElementById('modal-overlay').style.display = 'block';
+    document.getElementById('modal-overlay').style.display = 'flex';
+    document.getElementById('modal-overlay').style.justifyContent = 'center';
+    document.getElementById('modal-overlay').style.alignItems = 'center';
+    document.getElementById('modal').style.display = 'block';
 });
 
 document.getElementById('modal-overlay').addEventListener('click', function (e) {
     if (e.target.id === 'modal-overlay') {
         console.log('Overlay clicked'); // Debugging
         document.getElementById('modal-overlay').style.display = 'none';
+        document.getElementById('modal').style.display = 'none';
     }
 });
